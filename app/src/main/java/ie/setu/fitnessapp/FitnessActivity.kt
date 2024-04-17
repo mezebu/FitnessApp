6680d5895f0d2541d74c29ef3e94ae1941257aa8
Ezeigwe Henry <65811270+Mezebu@users.noreply.github.com>
2024-04-17 16:59:21 +0100

    Implement navigation drawer and fragment switching in FitnessActivity
    This commit adds functionality to FitnessActivity to incorporate a navigation drawer and handle fragment switching. The navigation drawer allows users to navigate between different fragments within the app, such as the HomeFragment and ProfileFragment. It also includes a method to handle the addition of fitness activities by navigating to the AddFitnessActivity upon clicking the FloatingActionButton. Additionally, it ensures proper handling of back button presses to close the navigation drawer when open.

--- app/src/main/java/ie/setu/fitnessapp/FitnessActivity.kt
+++ app/src/main/java/ie/setu/fitnessapp/FitnessActivity.kt
@@ -1,16 +1,25 @@
 package ie.setu.fitnessapp
 
 
-import android.content.Intent
+import HomeFragment
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import ie.setu.fitnessapp.activities.AddFitnessActivity
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentTransaction
+import com.google.android.material.navigation.NavigationView
 import ie.setu.fitnessapp.databinding.ActivityMainBinding
+import ie.setu.fitnessapp.fragments.ProfileFragment
 import timber.log.Timber
 import timber.log.Timber.i
 
-class FitnessActivity : AppCompatActivity() {
+class FitnessActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
 
+    private lateinit var drawerLayout: DrawerLayout
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,14 +30,42 @@ class FitnessActivity : AppCompatActivity() {
         Timber.plant(Timber.DebugTree())
         i("Fitness Journal Activity started..")
 
-        binding.fab.setOnClickListener {
-            navigateToAddFitness()
+        drawerLayout = findViewById<DrawerLayout>(R.id.drawerLayout)
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+
+        setSupportActionBar(toolbar)
+        binding.navView.setNavigationItemSelectedListener(this)
+        val toggle = ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open_nav, R.string.close_nav)
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        if (savedInstanceState == null) {
+            replaceFragment(HomeFragment())
+            binding.navView.setCheckedItem(R.id.navHome)
         }
     }
 
-    private fun navigateToAddFitness() {
-        val intent = Intent(this, AddFitnessActivity::class.java)
-        startActivity(intent)
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.navHome -> replaceFragment(HomeFragment())
+            R.id.navProfile -> replaceFragment(ProfileFragment())
+        }
+        drawerLayout.closeDrawer(GravityCompat.START)
+        return true
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        val transaction: FragmentTransaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.fragmentContainer, fragment)
+        transaction.commit()
+    }
+
+    override fun onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)){
+            drawerLayout.closeDrawer(GravityCompat.START)
+        } else {
+            onBackPressedDispatcher.onBackPressed()
+        }
     }
 
 }
