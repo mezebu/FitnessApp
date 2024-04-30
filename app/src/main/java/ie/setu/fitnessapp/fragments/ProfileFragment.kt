afa6676295a261baef2d0ab0106a41b8651e7dce
Ezeigwe Henry <65811270+Mezebu@users.noreply.github.com>
2024-04-30 12:43:03 +0100

    Update ProfileFragment.kt

--- app/src/main/java/ie/setu/fitnessapp/fragments/ProfileFragment.kt
+++ app/src/main/java/ie/setu/fitnessapp/fragments/ProfileFragment.kt
@@ -5,13 +5,58 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import ie.setu.fitnessapp.R
+import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import ie.setu.fitnessapp.databinding.FragmentProfileBinding
+
 class ProfileFragment : Fragment() {
+    private var _binding: FragmentProfileBinding? = null
+    private val binding get() = _binding!!
+
+    private lateinit var firebaseAuth: FirebaseAuth
+    private lateinit var firestore: FirebaseFirestore
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+    ): View {
+        _binding = FragmentProfileBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        firebaseAuth = FirebaseAuth.getInstance()
+        firestore = FirebaseFirestore.getInstance()
+
+        val currentUser = firebaseAuth.currentUser
+        if (currentUser != null) {
+            firestore.collection("users").document(currentUser.uid)
+                .get()
+                .addOnSuccessListener { document ->
+                    if (document.exists()) {
+                        binding.textViewUsername.text = document.getString("username")
+                        binding.textViewEmail.text = currentUser.email // Email directly from Auth
+                    } else {
+                        // Handle no document found
+                        showErrorSnackbar("Login failed")
+                    }
+                }
+                .addOnFailureListener {
+                    // Handle errors
+                    showErrorSnackbar("Please enter email and password")
+                }
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    private fun showErrorSnackbar(message: String) {
+        Snackbar.make(binding.root, message, Snackbar.LENGTH_SHORT).show()
     }
 }
