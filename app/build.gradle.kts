b89be485abdc94321dad74753453298783b1c287
Ezeigwe Henry <65811270+Mezebu@users.noreply.github.com>
2024-05-02 22:33:30 +0100

    Update build.gradle.kts
    Updated dependencies:
    - Updated androidx.appcompat:appcompat to version 1.6.1
    - Updated com.google.android.material:material to version 1.12.0
    - Updated com.google.firebase:firebase-auth to version 22.3.1
    - Updated com.google.firebase:firebase-firestore to version 24.11.1
    - Added com.github.ajalt:timberkt:1.5.1
    - Added androidx.core:core-splashscreen:1.0.1
    - Added com.squareup.picasso:picasso:2.71828
    - Added com.github.bumptech.glide:glide:4.13.2 and its compiler

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,14 +44,14 @@ dependencies {
 
     implementation("androidx.core:core-ktx:1.13.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.11.0")
+    implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("com.google.firebase:firebase-auth:22.3.1")
     implementation("com.google.firebase:firebase-firestore:24.11.1")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    implementation("com.google.android.material:material:1.11.0")
+    implementation("com.google.android.material:material:1.12.0")
     implementation("com.github.ajalt:timberkt:1.5.1")
     implementation("androidx.core:core-splashscreen:1.0.1")
     implementation ("com.google.android.material:material:<version>")
@@ -59,5 +59,10 @@ dependencies {
     implementation("com.google.android.gms:play-services-auth:21.1.0")
     implementation ("com.google.firebase:firebase-storage:20.3.0")
     implementation("com.google.firebase:firebase-storage-ktx:20.3.0")
+    implementation ("com.squareup.picasso:picasso:2.71828")
+    implementation ("com.github.bumptech.glide:glide:4.13.2")
+    annotationProcessor ("com.github.bumptech.glide:compiler:4.13.2")
+
+
 
 }
